6691d6a271ffbcd0d55d68c6170c317b3d4ed04d
Karim Galal <kgalal@student.ethz.ch>
2022-10-16 01:48:03 +0200

    updates

--- ACETH/__init__.py
+++ ACETH/__init__.py
@@ -1,5 +1,7 @@
-__version__ = "0.0.8"
-__author__ = "Karim Galal and Walid Chat"
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+__version__ = "0.0.13"
+__author__ = "Karim Galal and Walid Chatt"
 __credits__ = "Analytics Club ETHZ"
 
 
--- ACETH/chatbot.py
+++ ACETH/chatbot.py
@@ -1,62 +1,79 @@
 import emoji,os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
 import torch
 from transformers import logging
 import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
-
 import warnings
 
 warnings.filterwarnings("ignore")
-print("Loading models...")
-logging.disable_progress_bar()
-loc = os.path.dirname(__file__)
-token_chat = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-model_chat = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
-with open(loc+'/data/token_emoji.pkl','rb') as f:
-    token_em = pickle.load(f)
-with open(loc+'/data/encoder_emoji.pkl','rb') as f:
-    encod_em = pickle.load(f)
 
-model_emoji = load_model(loc+'/data/emoji_model.h5')
-print('Model and tokenizer is loaded')
+class chatbot:
+    def __init__(self,emoji=True):
+        self.emoji = emoji
+        print('Loading model and other dependecies..',sep='')
+        logging.disable_progress_bar()
+        loc = os.path.dirname(__file__)
+        model = 'facebook/blenderbot-400M-distill'
+        self.token_chat = BlenderbotTokenizer.from_pretrained("facebook/blenderbot-400M-distill")
+        self.model_chat = BlenderbotForConditionalGeneration.from_pretrained("facebook/blenderbot-400M-distill")
 
-def addEmoji(text):
-    x = pad_sequences(token_em.texts_to_sequences([text]),maxlen=30)
-    y_hot = model_emoji.predict([x],verbose=0)[0]
-    w_sum = sum(y_hot)
-    sort = list(sorted(y_hot))
-    dummy = []
-    count = 0
-    for i in sort:
-        count+=i
-        dummy.append(count)
-    sort = dummy
-    r = np.random.uniform(0,1)
-    y = np.argmax(y_hot)
-    for i,w in enumerate(sort[:-1]):
-        if r <= w:
-            y = i
-            break
-    y = encod_em.inverse_transform([y])[0]
+        with open(loc+'/data/token_emoji.pkl','rb') as f:
+            self.token_em = pickle.load(f)
+        with open(loc+'/data/encoder_emoji.pkl','rb') as f:
+            self.encod_em = pickle.load(f)
 
-  
-    text = emoji.emojize(f'{text} :{y}:')
-    return text
+        self.model_emoji = load_model(loc+'/data/emoji_model.h5')
+        print('Model and tokenizer is loaded')
+        self.main()
 
-step = 0
-while 1:
-    user_text = input('>> User : ')
-    if user_text == ':q':
-        step = 0
-        break
-    new_user_input_ids = token_chat.encode(user_text + token_chat.eos_token, return_tensors='pt')
-    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-    chat_history_ids = model_chat.generate(bot_input_ids, max_length=1000, pad_token_id=token_chat.eos_token_id)
-    text = token_chat.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-    text = addEmoji(text)
-    step+=1
-    print('ACE chatbot : {}'.format(text))
+    def addEmoji(self,text):
+        x = pad_sequences(self.token_em.texts_to_sequences([text]),maxlen=30)
+        y_hot = self.model_emoji.predict([x],verbose=0)[0]
+        w_sum = sum(y_hot)
+        sort = list(sorted(y_hot))
+        dummy = []
+        count = 0
+        for i in sort:
+            count+=i
+            dummy.append(count)
+        sort = dummy
+        r = np.random.uniform(0,1)
+        y = np.argmax(y_hot)
+        for i,w in enumerate(sort[:-1]):
+            if r <= w:
+                y = i
+                break
+        y = self.encod_em.inverse_transform([y])[0]
+
+      
+        text = emoji.emojize(f'{text} :{y}:')
+        return text
+    def main(self): 
+        torch.manual_seed(42)
+        last = '-1'
+        while 1:
+            user_text = input('>> User : ')
+            if user_text == ':q':
+                break
+            inputs = self.token_chat([user_text], return_tensors='pt')
+            reply_ids = self.model_chat.generate(**inputs,max_length=30)
+
+            
+            text = self.token_chat.batch_decode(reply_ids, skip_special_tokens=True)[0]            
+            if last in text or text in last or 'bye' in text or 'ok' in text or 'alright' in text:
+                last = '-1'
+            else:
+                last = text
+            if self.emoji and ':D' not in text and ':)' not in text:
+                text = self.addEmoji(text)
+            
+            print('ACE chatbot : {}'.format(text))
+        s = input("You wanna talk to me again? (Y/n)")
+        if s == 'Y':
+            self.main()
+            return
+        
+        
